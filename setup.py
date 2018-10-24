960254cd3f8b246a6631364d2b3f4fb370418190
AndyTsangChun <atc1992andy@gmail.com>
2018-10-24 17:54:18 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -13,16 +13,7 @@ cwd = os.path.dirname(os.path.abspath(__file__))
 
 REQUIRED_PACKAGES = [
 	'argparse>=1.1',
-    'dill==0.2.7.1',
-	'fire >= 0.1.3',
-	'matplotlib >= 2.2.2',
-	'psutil >= 5.4.5',
-	'requests >= 2.18.4',
-	'scikit-image >= 0.13.1',
-	'scipy >= 1.0.1',
-	'tqdm >= 4.23.4',
-	'imutils >= 0.4.6',
-	'googledrivedownloader >= 0.2'
+    'numpy==1.14.2'
 ]
 
 setup(name='py_util',
